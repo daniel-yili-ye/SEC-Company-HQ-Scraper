3f9fbcfffeee617738f85428d8d23cde72e55ed7
Daniel Ye <daniel.ye@live.com>
2021-07-21 17:30:26 -0400

    readme updated

---
+++ README.md
@@ -0,0 +1,35 @@
+# SEC Web Scrapping Tool
+
+A command-line tool to match CIKs (Central Index Keys) to company headquarter locations from the [SEC EDGAR website](https://www.sec.gov/edgar/searchedgar/companysearch.html).
+
+## Installation
+
+Clone this repository.
+
+```bash
+git clone https://github.com/daniel-yili-ye/SEC-web-scrapping
+```
+
+Install dependencies.
+
+```bash
+pip install -r requirements.txt
+```
+
+## Usage
+
+Before running the program in the command-line, please save your input file in the same folder where the `script.py` file is located.
+
+If possible, please specify input csv and output csv filenames, otherwise the program will assume the file names listed in the optional parameters below. The input file must also contain `CIK` and `Year` columns.
+
+```bash
+python3 script.py [input.csv] [output.csv]
+```
+
+The script will include `City`, `new_state`, `SEC Filing`, `Form Type`, and `Date Filed` columns in the output file.
+
+## Contributing
+Pull requests are welcome. For major changes, please open an issue first to discuss what you would like to change.
+
+## License
+[MIT](https://choosealicense.com/licenses/mit/)
--- script.py
+++ script.py
@@ -1,29 +1,37 @@
-# import our libraries
+# import libraries
+import sys
 import requests
 import pandas as pd
 from bs4 import BeautifulSoup
 
 # import missing cities file
-df = pd.read_csv(r'missing.csv')
+try:
+    input = sys.argv[1]
+except:
+    input = 'input.csv'
+
+try:
+    output = sys.argv[2]
+except:
+    output = 'output.csv'
+
+df = pd.read_csv(input)
 
 # create CIK list
 CIK_list = []
-
 for index, row in df.iterrows():
     CIK_list.append(row['CIK'])
 
 # create year list
 year_list = []
-
 for index, row in df.iterrows():
     year_list.append(row['Year'])
 
 # determines whether mail line contains city and state
+
+
 def address_zip(x):
-    
-    # done to make the input easier to search
     y = (x[::-1]).strip()
-    
     try:
         if (y[:5].isnumeric() and y[5] == ' ' and y[6:8].isalpha() and y[8] == ' '):
             return True
@@ -33,11 +41,10 @@ def address_zip(x):
         return False
 
 # alternate formula to determines whether mail line contains city and state
+
+
 def address_zip2(x):
-    
-    # done to make the input easier to search
     y = (x[::-1]).strip()
-    
     try:
         if (y[:4].isnumeric() and y[4] == '-' and y[5:10] and y[10] == ' ' and y[11:13].isalpha() and y[13] == ' '):
             return True
@@ -45,6 +52,7 @@ def address_zip2(x):
             return False
     except:
         return False
+
 
 # base URL for the SEC EDGAR browser
 endpoint = r"https://www.sec.gov/cgi-bin/browse-edgar"
@@ -59,23 +67,23 @@ date_list = []
 for i in range(len(CIK_list)):
 
     # define our parameters dictionary
-    param_dict = {'action':'getcompany',
+    param_dict = {'action': 'getcompany',
                   'CIK': CIK_list[i],
-                  'type':'10-Q',
+                  'type': '10-Q',
                   'dateb': str(year_list[i]) + '1231',
-                  'owner':'exclude',
-                  'output':'atom',
-                  'count':'10'}
-    
+                  'owner': 'exclude',
+                  'output': 'atom',
+                  'count': '10'}
+
     # request the url, and then parse the response
-    response = requests.get(url = endpoint, params = param_dict)
+    response = requests.get(url=endpoint, params=param_dict)
     soup = BeautifulSoup(response.content, 'lxml')
     entry = soup.find('entry')
 
     # find the latest entry in the year
     if entry == None:
         param_dict['type'] = ''
-        response = requests.get(url = endpoint, params = param_dict)
+        response = requests.get(url=endpoint, params=param_dict)
         soup = BeautifulSoup(response.content, 'lxml')
         entry = soup.find('entry')
 
@@ -83,8 +91,9 @@ for i in range(len(CIK_list)):
     if entry == None:
         try:
             param_dict['dateb'] = str(year_list[i] + 1) + '1231'
-            param_dict['count'] = '100' # 100 is the highest value possible for count
-            response = requests.get(url = endpoint, params = param_dict)
+            # 100 is the highest value possible for count
+            param_dict['count'] = '100'
+            response = requests.get(url=endpoint, params=param_dict)
             soup = BeautifulSoup(response.content, 'lxml')
             entry = soup.find_all('entry')[-1]
         except:
@@ -100,16 +109,16 @@ for i in range(len(CIK_list)):
         form = ''
         date = ''
 
-    # finding city & state 
+    # finding city & state
     if link == '':
         city = ''
         state = ''
     else:
         # request the link url to parse for city & state info
-        child_response = requests.get(url = link)
+        child_response = requests.get(url=link)
         child_soup = BeautifulSoup(child_response.content, 'html.parser')
         addresses = child_soup.find_all('div', id='filerDiv')
-        
+
         # to ensure the right company addresses are selected
         sub_string = str(CIK_list[i]) + ' (see all company filings)'
         for add in addresses:
@@ -123,11 +132,11 @@ for i in range(len(CIK_list)):
                 business_lines = c_add.find_all('span')
             elif 'Mailing Address' in str(c_add):
                 mailing_lines = c_add.find_all('span')
-   
+
         # assigning city & state
         city = ''
         state = ''
-        
+
         for line in business_lines:
             if address_zip(line.text):
                 city = line.text.strip()[:-9]
@@ -135,7 +144,7 @@ for i in range(len(CIK_list)):
             elif address_zip2(line.text):
                 city = line.text.strip()[:-14]
                 state = line.text.strip()[-13:-11]
-        
+
         if city == '' and state == '':
             for line in mailing_lines:
                 if address_zip(line.text):
@@ -144,8 +153,7 @@ for i in range(len(CIK_list)):
                 elif address_zip2(line.text):
                     city = line.text.strip()[:-14]
                     state = line.text.strip()[-13:-11]
-    
-    
+
     # assigning values to list
     city_list.append(city)
     state_list.append(state)
@@ -162,5 +170,4 @@ df['Form Type'] = form_list
 df['Date Filed'] = date_list
 
 # export dataset for manual analysis and to double check work
-file_name = 'out.csv'
-df.to_csv(file_name, encoding='utf-8', index=False)
+df.to_csv(output, encoding='utf-8', index=False)
